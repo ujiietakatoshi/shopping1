613fb7179205a35961b4c00145227a8f7b47ff39
ujiietakatoshi <fadeoutinu@gmail.com>
2020-03-23 21:30:40 +0900

    commit4

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -5,19 +5,44 @@
     <title>野菜市場</title>
   </head>
   <body>
-  スタッフ追加<br />
-  <br />
-  <form method="post" action="staff_add_check.php">
-  スタッフ名を入力してください<br />
-  <input type="text" name="name" style="width:200px"><br />
-  パスワードを入力してください。<br />
-  <input type="password" name="pass" style="width:100px"><br />
-  パスワードをもう一度入力してください。<br />
-  <input type="password" name="pass2" style="width:100px"><br />
-  <br />
-  <input type="button" onclick="history.back()" value="戻る">
-  <input type="submit" value="OK">
-  </form>
+
+  <?php
+
+  try
+  {
+  
+  $staff_name = $_POST['name'];
+  $staff_pass = $_POST['pass'];
+  
+  $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+  $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+  $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+  $user ='root';
+  $password ='';
+  $dbh = new PDO($dsn,$user,$password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  $sql = 'INSERT INTO mst_staff(name,password) VALUES(?,?)';
+  $stmt = $dbh->prepare($sql);
+  $data[] = $staff_name;
+  $data[] = $staff_pass;
+  $stmt->execute($data);
+
+  $dbh = null;
+
+  print $staff_name;
+  print'さんを追加しました。<br />';
+
+  }
+  catch(Exception $e)
+  {
+    print'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+  }
+
+  ?>
+
+  <a href="staff_list.php">戻る</a>
 
   </body>
 </html>
